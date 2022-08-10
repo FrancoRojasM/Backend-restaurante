bf75d09e039aad92ddedc4c41d6ca441cc2f7ec9
FrancoRM <franco_rojas_maravi@hotmail.com>
2022-08-10 03:49:10 -0500

    actualizacion aplicacion de gestionCategorias y adminUsuarios - autenticacion

--- APIrestaurante/APIrestaurante/urls.py
+++ APIrestaurante/APIrestaurante/urls.py
@@ -4,6 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gestioncategorias/',include('gestionCategorias.urls')),
-    path('adminuarios/',include('adminUsuarios.urls'))
-
+    path('adminusuarios/',include('adminUsuarios.urls'))
 ]
